79133578e97b6cbea4616507b885bc85f13d7116
dina-amelia <dinaamelia4124@gmail.com>
2021-12-23 14:58:57 +0700

    Day 4

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Role;
 use  Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
@@ -15,42 +16,60 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $admin = Role::create([
+            'name' => 'admin',
+            'display_name' => 'User Administrator'
+        ]);
+
+        $pengguna = Role::create([
+            'name' => 'pengguna',
+            'display_name' => 'User Biasa'
+        ]);
+
         $user = new User();
         $user->name = 'Dina Amelia';
         $user->email = 'admin@gmail.com';
         $user->password = Hash::make('12345678');
         $user->save();
 
-        $user = new User();
-        $user->name = 'Fitria Amelia';
-        $user->email = 'member@gmail.com';
-        $user->password = Hash::make('01234567');
-        $user->save();
+        $pengunjung = new User();
+        $pengunjung->name = 'Pengunjung';
+        $pengunjung->email = 'pengunjung@gmail.com';
+        $pengunjung->password = Hash::make('12345678');
+        $pengunjung->save();
 
-        $user = new User();
-        $user->name = 'Kesyza Andriana H';
-        $user->email = 'kesyza@gmail.com';
-        $user->password = Hash::make('23456789');
-        $user->save();
+        $user->attachRole($admin);
+        $pengunjung->attachRole($pengguna);
 
-        $user = new User();
-        $user->name = 'Sila Ramadina';
-        $user->email = 'sila@gmail.com';
-        $user->password = Hash::make('34567890');
-        $user->save();
+        // $user = new User();
+        // $user->name = 'Fitria Amelia';
+        // $user->email = 'member@gmail.com';
+        // $user->password = Hash::make('01234567');
+        // $user->save();
 
-        $user = new User();
-        $user->name = 'Rifa Fauziah';
-        $user->email = 'rifa@gmail.com';
-        $user->password = Hash::make('45678901');
-        $user->save();
+        // $user = new User();
+        // $user->name = 'Kesyza Andriana H';
+        // $user->email = 'kesyza@gmail.com';
+        // $user->password = Hash::make('23456789');
+        // $user->save();
 
-        $user = new User();
-        $user->name = 'Erin Rafani';
-        $user->email = 'erin@gmail.com';
-        $user->password = Hash::make('56789012');
-        $user->save();
+        // $user = new User();
+        // $user->name = 'Sila Ramadina';
+        // $user->email = 'sila@gmail.com';
+        // $user->password = Hash::make('34567890');
+        // $user->save();
 
+        // $user = new User();
+        // $user->name = 'Rifa Fauziah';
+        // $user->email = 'rifa@gmail.com';
+        // $user->password = Hash::make('45678901');
+        // $user->save();
+
+        // $user = new User();
+        // $user->name = 'Erin Rafani';
+        // $user->email = 'erin@gmail.com';
+        // $user->password = Hash::make('56789012');
+        // $user->save();
 
     }
 }
